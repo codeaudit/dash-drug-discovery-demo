2aadf3e9800d47902d54a916b095fa3069354ae2
alishobeiri <alishobeiri@ymail.com>
2017-06-20 16:20:12 -0400

    Logo top right

--- app.py
+++ app.py
@@ -297,8 +297,11 @@ app.layout = html.Div([
     html.Div([
         html.Img(src="https://cdn.rawgit.com/plotly/design-assets/master/logo/dash/images/dash-logo-by-plotly-stripe.png?token=ARkbw71c0vCS8Jnau4bHOoc9HRF_lZAtks5ZUrwDwA%3D%3D",
                 style={
-                    'height': '80px',
-                    'float': 'right'
+                    'height': '120px',
+                    'float': 'right',
+                    'position': 'relative',
+                    'bottom': '40px',
+                    'left': '50px'
                 },
                 ),
         html.H2('Dash',
